b981cb5a929698c760e9d0d00b13adb46097c3f7
Buggyeyes <ovel2405@yandex.ru>
2023-09-15 12:08:27 +0300

    1

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 // const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 module.exports = {
     mode: "development",
@@ -29,6 +30,12 @@ module.exports = {
         }),
             // new CleanWebpackPlugin(),
             new webpack.HotModuleReplacementPlugin(),
+            new CopyWebpackPlugin({
+                patterns: [{
+                    from: path.resolve(__dirname, './src/pics'),
+                    to: path.resolve(__dirname, './dist/pics')
+                }]
+            })
         ],
 
 
